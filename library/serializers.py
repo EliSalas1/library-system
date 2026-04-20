e7bc883b1143b4e6a438c05221072e616a15ae48
Eliza <elisalaschac05@gmail.com>
2026-04-19 20:05:45 -0500

    feat: implementacion de logica de loan con acciones especiales: Extend y return_book

--- library/serializers.py
+++ library/serializers.py
@@ -12,3 +12,20 @@ class LoanSerializer(serializers.ModelSerializer):
     class Meta:
         model = Loan
         fields = '__all__'
+
+    def validate(self, data):
+        book = data['book']
+
+        if book.available_copies <= 0:
+            raise serializers.ValidationError("No hay copias disponibles de este libro.")
+        
+        return data
+    
+    def create(self, validated_data):
+        book = validated_data['book']
+
+        #redducir copia disponible
+        book.available_copies -=1
+        book.save()
+
+        return super().create(validated_data)
--- library/views.py
+++ library/views.py
@@ -1,13 +1,61 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from .models import Book, Loan
 from .serializers import BookSerializer, LoanSerializer
-
-
-class BookViewSet(viewsets.ModelViewSet):
-    queryset = Book.objects.all()
-    serializer_class = BookSerializer
+from django.utils import timezone
+from datetime import timedelta
 
 
 class LoanViewSet(viewsets.ModelViewSet):
     queryset = Loan.objects.all()
     serializer_class = LoanSerializer
+
+    # 🔹 DEVOLVER LIBRO
+    @action(detail=True, methods=['post'])
+    def return_book(self, request, pk=None):
+        loan = self.get_object()
+
+        if loan.status == 'returned':
+            return Response({"error": "El libro ya fue devuelto"}, status=400)
+
+        loan.status = 'returned'
+        loan.return_date = timezone.now()
+        loan.save()
+
+        # devolver copia al inventario
+        loan.book.available_copies += 1
+        loan.book.save()
+
+        return Response({"message": "Libro devuelto correctamente"})
+
+    # 🔹 EXTENDER PRÉSTAMO
+    @action(detail=True, methods=['post'])
+    def extend(self, request, pk=None):
+        loan = self.get_object()
+
+        if loan.status != 'active':
+            return Response({"error": "Solo préstamos activos pueden extenderse"}, status=400)
+
+        loan.return_date = (loan.return_date or timezone.now()) + timedelta(days=7)
+        loan.save()
+
+        return Response({"message": "Préstamo extendido 7 días"})
+    
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        author = self.request.query_params.get('author')
+        year = self.request.query_params.get('year')
+
+        if author:
+            queryset = queryset.filter(author__icontains=author)
+
+        if year:
+            queryset = queryset.filter(year=year)
+
+        return queryset
+
